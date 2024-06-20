329fbdcf0e63b36a8ff314e5c1244c01aec5528a
ramk4488 <ramkmurthy4488@gmail.com>
2024-06-20 11:41:02 +0530

    Search Products from API and list

---
+++ Fetch-API-search-result/main.js
@@ -0,0 +1,50 @@
+// let apiResponse = fetch('https://fakestoreapi.com/products');
+// let resposeObj;
+// apiResponse.then(res => res.json())
+//                 .then(data => {
+//                     resposeObj = data;
+//                 }).then(() => {
+//                     console.log("data", resposeObj);
+//                 });
+//                 console.log("data", resposeObj);
+
+// fetch('https://fakestoreapi.com/products')
+//   .then(res => res.json())
+//   .then(data => {
+//     obj = data;
+//    })
+//   .then(() => {
+//     console.log(obj);
+//    });
+
+
+async function fetchDataAndShowResult(searcData = null) {
+    let resposeObj;
+    const res = await fetch('https://fakestoreapi.com/products');
+    resposeObj = await res.json();
+    const parentDiv = document.getElementById('show-result');
+    console.log(resposeObj)
+    parentDiv.innerHTML = "";
+    for (let i = 0; i < resposeObj.length; i++) {
+        console.log(searcData)
+        if(searcData != null){
+            if(resposeObj[i].title.includes(searcData)){
+                const productTitleDiv = document.createElement('div');
+                productTitleDiv.innerHTML = '<span><img style="height:50px; width:50px;" src="'+resposeObj[i].image+'" alt=""></span><span>'+resposeObj[i].title+'</span>';
+                parentDiv.append(productTitleDiv)
+            }
+        }else{
+            const productTitleDiv = document.createElement('div');
+            productTitleDiv.innerHTML = '<span><img style="height:50px; width:50px;" src="'+resposeObj[i].image+'" alt=""></span><span>'+resposeObj[i].title+'</span>';
+            parentDiv.appendChild(productTitleDiv)
+
+        }
+        
+    }
+}
+fetchDataAndShowResult()
+const searchField = document.getElementById('search');
+searchField.addEventListener('keyup', function(data){
+    searcData = searchField.value;
+    fetchDataAndShowResult(searcData)
+})
