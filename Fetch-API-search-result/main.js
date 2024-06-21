c87fe57833dc927b6e9377da1ceed35894504c8c
ramk4488 <ramkmurthy4488@gmail.com>
2024-06-21 12:06:49 +0530

    Add products to to favorite list

--- Fetch-API-search-result/main.js
+++ Fetch-API-search-result/main.js
@@ -19,24 +19,40 @@
 
 
 async function fetchDataAndShowResult(searcData = null) {
+    // localStorage.removeItem('fav-product-id');
     let resposeObj;
-    const res = await fetch('https://fakestoreapi.com/products');
+    const res = await fetch('https://dummyjson.com/products');
     resposeObj = await res.json();
     const parentDiv = document.getElementById('show-result');
-    console.log(resposeObj)
+    const favProducts = document.getElementById('favProducts');
+    console.log(resposeObj.products)
     parentDiv.innerHTML = "";
-    for (let i = 0; i < resposeObj.length; i++) {
-        console.log(searcData)
-        if(searcData != null){
-            if(resposeObj[i].title.toLowerCase().includes(searcData.toLowerCase())){
+    favProducts.innerHTML = "";
+    for (let i = 0; i < resposeObj.products.length; i++) {
+        const existingData = localStorage.getItem('fav-product-id');
+        let favIcon = '<span onclick="addToFav('+resposeObj.products[i].id+')" class="add-to-fav" style="font-size:200%;color:green;">&hearts;</span>';
+        if(existingData){
+            const dataArray = JSON.parse(existingData);
+            const index = dataArray.indexOf(String(resposeObj.products[i].id));
+            // alert(index);
+            if (index > -1) { 
+            // if(existingData.includes(String(resposeObj.products[i].id))){
+                favIcon = '<span onclick="removeFromFav('+resposeObj.products[i].id+')" class="remove-to-fav" style="font-size:200%;color:red;">&hearts;</span>';
                 const productTitleDiv = document.createElement('div');
-                productTitleDiv.innerHTML = '<span><img style="height:50px; width:50px;" src="'+resposeObj[i].image+'" alt=""></span><span>'+resposeObj[i].title+'</span>';
-                parentDiv.append(productTitleDiv)
+                productTitleDiv.innerHTML = '<span class="prod-img"><img style="height:50px; width:50px;" src="'+resposeObj.products[i].images+'" alt=""></span><span class="product-title">'+resposeObj.products[i].title+'</span>'+favIcon;
+                favProducts.append(productTitleDiv)
+            }
+        }
+        if(searcData != null){
+            if(resposeObj.products[i].title.toLowerCase().includes(searcData.toLowerCase())){
+                const productTitleDiv = document.createElement('div');
+                productTitleDiv.innerHTML = '<span class="prod-img"><img style="height:50px; width:50px;" src="'+resposeObj.products[i].images+'" alt=""></span><span class="product-title">'+resposeObj.products[i].title+'</span>'+favIcon;
+                parentDiv.append(productTitleDiv);
             }
         }else{
             const productTitleDiv = document.createElement('div');
-            productTitleDiv.innerHTML = '<span><img style="height:50px; width:50px;" src="'+resposeObj[i].image+'" alt=""></span><span class="product-title">'+resposeObj[i].title+'</span>';
-            parentDiv.appendChild(productTitleDiv)
+            productTitleDiv.innerHTML = '<span class="prod-img"><img style="height:50px; width:50px;" src="'+resposeObj.products[i].images+'" alt=""></span><span class="product-title">'+resposeObj.products[i].title+'</span>'+favIcon;
+            parentDiv.appendChild(productTitleDiv);
 
         }
         
@@ -48,3 +64,30 @@ searchField.addEventListener('keyup', function(data){
     searcData = searchField.value;
     fetchDataAndShowResult(searcData)
 })
+
+function addToFav(productId){
+    const existingData = localStorage.getItem('fav-product-id');
+    const dataArray = [];
+    if(existingData){
+        const dataArray = JSON.parse(existingData);
+        dataArray.push(String(productId));
+        localStorage.setItem('fav-product-id', JSON.stringify(dataArray));
+    }else{
+        dataArray.push(String(productId));
+        localStorage.setItem('fav-product-id', JSON.stringify(dataArray));
+    }
+    fetchDataAndShowResult();
+}
+
+function removeFromFav(productId){
+    const existingData = localStorage.getItem('fav-product-id');
+    if(existingData){
+        const dataArray = JSON.parse(existingData);
+        const index = dataArray.indexOf(String(productId));
+        if (index > -1) {
+            dataArray.splice(index, 1);
+        }
+        localStorage.setItem('fav-product-id', JSON.stringify(dataArray));
+    }
+    fetchDataAndShowResult();
+}
